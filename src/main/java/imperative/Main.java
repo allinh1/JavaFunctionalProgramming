6b9f9d54c7a9dc81eee1b6585f116bbc9967505a
allinh1 <84691465+allinh1@users.noreply.github.com>
2021-09-21 12:39:25 +1000

    declarative vs imperative

--- src/main/java/imperative/Main.java
+++ src/main/java/imperative/Main.java
@@ -1,25 +1,42 @@
 package imperative;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static imperative.Gender.FEMALE;
+import static imperative.Gender.MALE;
 
 public class Main {
     public static void main(String[] args) {
         List<Person> people = List.of(
-
+                new Person("John", MALE),
+                new Person("Sean", MALE),
+                new Person("Aisha", FEMALE),
+                new Person("Alex", MALE),
+                new Person("Alice", FEMALE)
         );
-    }
-        static class Person {
-            private final String name;
-            private final Gender gender;
 
-            Person(String name, Gender gender) {
-                this.name = name;
-                this.gender = gender;
+        // Imperative approach
+        System.out.println("Imperative approach");
+
+        List<Person> females = new ArrayList<>(); // empty list
+        for (Person person : people) {
+            if (FEMALE.equals(person.gender)) { // if ,equals logic
+                females.add(person);
             }
         }
-
-        enum Gender {
-        MALE, FEMALE
+        for (Person female: females) {  // final for loop for print
+            System.out.println(female);
         }
+
+        // Declarative approach
+        System.out.println("Declarative approach");
+        List<Person> females2 = people.stream()
+                .filter(person -> FEMALE.equals(person.gender))
+                .collect(Collectors.toList());
+        females2.forEach(System.out::println);
+
     }
+}
 
---
+++ src/main/java/imperative/Person.java
@@ -0,0 +1,20 @@
+package imperative;
+
+public class Person {
+        private final String name;
+        public final Gender gender;
+
+        Person(String name, Gender gender) {
+            this.name = name;
+            this.gender = gender;
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    ", gender=" + gender +
+                    '}';
+        }
+    }
+
