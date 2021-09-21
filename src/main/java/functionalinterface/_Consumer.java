06c24eca01e0f2a1a3956b37e94d0f5a7506ddba
allinh1 <84691465+allinh1@users.noreply.github.com>
2021-09-21 14:55:50 +1000

    BiFunctions

---
+++ src/main/java/functionalinterface/_Consumer.java
@@ -0,0 +1,56 @@
+package functionalinterface;
+
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+
+public class _Consumer {
+    public static void main(String[] args) {
+        // Normal Java Function
+        Customer steffie = new Customer("Steffie", "0404123456");
+        greetCustomer(steffie);
+
+        //BiConsumerTest
+        greetCustomerV2(steffie, false);
+
+        // Consumer Functional Interface
+        greetCustomerConsumer.accept(steffie);
+
+        // Using BiConsumer
+        greetCustomerConsumerV2.accept(steffie, true);
+
+        greetCustomerConsumerV2.accept(steffie, false);
+
+    }
+
+    static BiConsumer<Customer, Boolean> greetCustomerConsumerV2 = (customer, showPhoneNumber) ->
+            System.out.println("Hello " + customer.customerName +
+                    ", thanks for registering phone number " +
+                    (showPhoneNumber ? customer.customerPhoneNumber : "**********"));
+
+    static Consumer<Customer> greetCustomerConsumer = customer ->
+            System.out.println("Hello " + customer.customerName +
+                    ", thanks for registering phone number " +
+                    customer.customerPhoneNumber);
+
+    static void greetCustomer(Customer customer) {
+        System.out.println("Hello " + customer.customerName +
+                ", thanks for registering phone number " +
+                customer.customerPhoneNumber);
+    }
+
+    static void greetCustomerV2(Customer customer, boolean showPhoneNumber) {
+        System.out.println("Hello " + customer.customerName +
+                ", thanks for registering phone number " +
+                (showPhoneNumber ? customer.customerPhoneNumber : "**********"));
+    }
+
+    static class Customer {
+        private final String customerName;
+        private final String customerPhoneNumber;
+
+        public Customer(String customerName, String customerPhoneNumber) {
+            this.customerName = customerName;
+            this.customerPhoneNumber = customerPhoneNumber;
+        }
+    }
+}
