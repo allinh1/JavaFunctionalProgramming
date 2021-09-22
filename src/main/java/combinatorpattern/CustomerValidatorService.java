b8d850fe3fa047c8acc767d700538da7a92bb484
allinh1 <84691465+allinh1@users.noreply.github.com>
2021-09-22 10:23:39 +1000

    Combinator Pattern Validation added

--- src/main/java/combinatorpattern/CustomerValidatorService.java
+++ src/main/java/combinatorpattern/CustomerValidatorService.java
@@ -22,7 +22,4 @@ public class CustomerValidatorService {
                 isPhoneNumberValid(customer.getPhoneNumber()) &&
                 isAdult(customer.getDob());
     }
-
-
-
 }
--- src/main/java/combinatorpattern/Main.java
+++ src/main/java/combinatorpattern/Main.java
@@ -2,6 +2,8 @@ package combinatorpattern;
 
 import java.time.LocalDate;
 
+import static combinatorpattern.CustomerRegistrationValidator.*;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -9,13 +11,26 @@ public class Main {
                 "Alice",
                 "test@test.com",
                 "+61404123123",
-                LocalDate.of(2000,1,1)
+                LocalDate.of(1991,1,1)
         );
 
-        System.out.println(new CustomerValidatorService().isValid(customer));
-
-        // valid customer = true , store customer in db.
 
 
+//        System.out.println(new CustomerValidatorService().isValid(customer));
+
+//         valid customer = true , store customer in db.
+
+
+        // Using combinator pattern
+        ValidationResult result = isEmailValid()
+                .and(isPhoneValid())
+                .and(isAnAdult())
+                .apply(customer);
+
+        System.out.println(result);
+
+        if (result != ValidationResult.SUCCESS) {
+            throw new IllegalStateException(result.name());
+        }
     }
 }
